7506836ec3d5c613b7bb30564a5446f5b19d45ed
Maayan Keenan <maayan.keenan@capriza.com>
2015-02-15 15:39:37 +0200

    adding timers

--- js/views/main.js
+++ js/views/main.js
@@ -2,11 +2,18 @@ var AppView = Backbone.View.extend({
     el: '.dynamic-view',
 
     initialize: function(){
+        // recurring view
         this.minus7View = new Minus7View();
         this.nounsView = new NounsView();
+
+        // defining our views
         this.views = [new TimeView(), this.minus7View,  new LocationView(), this.nounsView, this.minus7View, new PicturesView(), this.nounsView, new HourView(), new AnalogWatchView()];
+
         this.total = 0;
         var self = this;
+        this.statisticsMatrix = [];
+
+        // register to events
         $('#startBtn').on('click', function(){
             if($('#idBox').val() != '') {
                 this.startTime = new Date();
@@ -23,15 +30,39 @@ var AppView = Backbone.View.extend({
         this.$el.empty();
         $('.actions').show();
         this.viewIndex = 0;
+
+        // render first view
         this.views[this.viewIndex].render();
+
+        // start timer
+        this.startTime = new Date();
     },
 
     doNext: function() {
         if(this.views[this.viewIndex].canContinue() && this.viewIndex < this.views.length) {
-            this.total += this.views[this.viewIndex].getScore();
+
+            // stop timer
+            this.endTime = new Date();
+
+            // get current view score
+            var currentScore = this.views[this.viewIndex].getScore();
+
+            // save in matrix: score and time by view num
+            this.statisticsMatrix[this.viewIndex] = {score: currentScore, time: this.endTime - this.startTime};
+
+            // sum total score
+            this.total += currentScore;
+
+            // continue to next view
             ++this.viewIndex;
-            console.log(this.total);
             this.views[this.viewIndex].render();
+
+            // start new timer
+            this.startTime = new Date();
         }
+    },
+
+    doFinish : function() {
+      // save matrix with user is and total score in file
     }
 });
